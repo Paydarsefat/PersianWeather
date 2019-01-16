2d3c424db83fc6b3fde7ae3c384ff3eb7d651621
m.sharmsar@gmail.com <m.sharmsar@gmail.com>
2019-01-16 23:30:42 +0330

    first step to fetch api  weather

--- app/screens/Homescreen.js
+++ app/screens/Homescreen.js
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Button} from "react-native";
-
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
 
 export default class Homescreen extends Component {
   constructor(props) {
     super(props);
     var navigation = this.props.navigation;
     // this.state = {};
+    this.fetchCityTemp("mashhad", "IR");
   }
+
+  fetchCityTemp = (city, country) => {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "," +
+        country +
+        "&appid=5e35e77dd812ab43a4214216d2ff9383&units=metric"
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        var r = responseJson.main;
+        var obj = responseJson;
+        var city = {
+          name: obj.name,
+          country: country,
+          temp: Math.ceil(r.temp),
+          type: obj.weather[0].main
+        };
+
+        Alert.alert("your OK! when Mount class");
+      });
+  };
+
+  showAlert = () => {
+    Alert.alert("your OK ! After press Button");
+  };
+
   render() {
-    return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button title="Go to Details" onPress={() => this.props.navigation.navigate("Detial")} />
-        <Text>Homescreen</Text>
-      </View>;
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate("Detial")}
+        />
+        <Button title="Alert Button" onPress={this.showAlert} />
+        <Text>Homescreen </Text>
+      </View>
+    );
   }
 }
